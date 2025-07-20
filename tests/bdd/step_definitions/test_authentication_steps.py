65de508b132023e6de74e5249e5d91c6847e0538
Carlos Roberto <carlosjr.if@gmail.com>
2025-07-19 21:59:12 -0300

    tests: test if db session is ready

--- tests/bdd/step_definitions/test_authentication_steps.py
+++ tests/bdd/step_definitions/test_authentication_steps.py
@@ -1,5 +1,5 @@
 import pytest
-from pytest_bdd import scenarios
+from pytest_bdd import scenarios, given
 from tests.bdd.support.auth_helpers import AuthTestHelper
 
 scenarios('../features/authentication.feature')
@@ -7,3 +7,7 @@ scenarios('../features/authentication.feature')
 @pytest.fixture
 def auth_helper(db_session):
     return AuthTestHelper(db_session)
+
+@given('que o sistema está funcionando')
+def system_is_running(db_session):
+    assert db_session is not None
